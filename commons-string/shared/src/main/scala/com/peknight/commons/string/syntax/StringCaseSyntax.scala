3745b1269e55512ff3dc1b1b30ee8fa84ba1893a
peknight <JKpeknight@gmail.com>
2024-04-20 16:15:12 +0800

    update

--- commons-string/shared/src/main/scala/com/peknight/commons/string/syntax/StringCaseSyntax.scala
+++ commons-string/shared/src/main/scala/com/peknight/commons/string/syntax/StringCaseSyntax.scala
@@ -6,7 +6,7 @@ trait StringCaseSyntax:
   extension (value: String)
     def capitalize: String = StringCaseOps.capitalize(value)
     def to(stringCase: StringCase): String = StringCaseOps.to(value, stringCase)
-    def split: Seq[String] = StringCaseOps.split(value)
+    def splitByCase: Seq[String] = StringCaseOps.split(value)
   end extension
 end StringCaseSyntax
 object StringCaseSyntax extends StringCaseSyntax
