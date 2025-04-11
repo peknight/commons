8ebc3778c9ec60eddcb0e214ce342edd1249d013
peknight <JKpeknight@gmail.com>
2025-04-11 15:53:40 +0800

    update

--- commons-time/shared/src/main/scala/com/peknight/commons/time/syntax/InstantSyntax.scala
+++ commons-time/shared/src/main/scala/com/peknight/commons/time/syntax/InstantSyntax.scala
@@ -6,6 +6,8 @@ import scala.concurrent.duration.*
 trait InstantSyntax:
   extension (instant: Instant)
     def toDuration: FiniteDuration = instant.getEpochSecond.seconds + instant.getNano.nanos
+    def minus(that: Instant): FiniteDuration = toDuration - that.toDuration
+    def -(that: Instant): FiniteDuration = minus(that)
   end extension
 end InstantSyntax
 object InstantSyntax extends InstantSyntax
--- commons-time/shared/src/main/scala/com/peknight/commons/time/syntax/TemporalSyntax.scala
+++ commons-time/shared/src/main/scala/com/peknight/commons/time/syntax/TemporalSyntax.scala
@@ -7,7 +7,9 @@ import scala.jdk.DurationConverters.*
 trait TemporalSyntax:
   extension [T <: Temporal] (temporal: T)
     def plus(duration: FiniteDuration): T = temporal.plus(duration.toJava).asInstanceOf[T]
+    def +(duration: FiniteDuration): T = plus(duration)
     def minus(duration: FiniteDuration): T = temporal.minus(duration.toJava).asInstanceOf[T]
+    def -(duration: FiniteDuration): T = minus(duration)
   end extension
 end TemporalSyntax
 object TemporalSyntax extends TemporalSyntax
